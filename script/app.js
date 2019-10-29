0838b3210808325930ed3004d3d592fc73b1855d
kharyna88 <kharyna88@gmail.com>
2019-10-29 02:15:37 +0200

    add weather API and displayed the weather

--- script/app.js
+++ script/app.js
@@ -20,6 +20,24 @@ function geocode(event) {
 
             console.log(lat, lng);
 
+            let locationText = document.querySelector('.location-text');
+            locationText.textContent = response.data.results[0].formatted_address;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            let api = `${proxy}http://api.weatherstack.com/current?access_key=c4db8878e27270c7e233fe2980948cff&query=${lat},${lng}`
+
+            axios(api)
+            .then(function (response) {
+                console.log(response);
+
+                let temperature = document.querySelector('.temperature');
+                temperature.textContent = response.data.current.temperature;
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+
         })
         .catch(function (error) {
             console.log(error);
