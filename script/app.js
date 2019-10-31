ad78f45c686726f69a702537687c0bbb78a97314
kharyna88 <kharyna88@gmail.com>
2019-10-31 02:44:14 +0200

    finish my weather app

--- script/app.js
+++ script/app.js
@@ -1,16 +1,22 @@
+const proxy = "https://cors-anywhere.herokuapp.com/"
 let locationForm = document.querySelector('#location-form');
 let locationText = document.querySelector('.location-text');
 let temperature = document.querySelector('.temperature');
+let temperatureText = document.querySelector('.temperature-text');
+let weatherIcon = document.querySelector('.weather-icon');
+let weatherDescription = document.querySelector('.weather-description');
+let windText = document.querySelector('.wind-text');
+let windSpeed = document.querySelector('.wind-speed');
+let myLocation = document.querySelector('.my-location');
+
 let latt;
 let long;
 
-let degree = document.createElement("span");
-degree.innerHTML = "C";
-degree.className = "degree"
+getLocation();
 
-// Listen for submit
-locationForm.addEventListener('submit', geocode);
+myLocation.addEventListener('click', getLocation);
 
+// get my location
 function getLocation() {
     return new Promise((resolve, reject) => {
         if (navigator.geolocation) {
@@ -21,35 +27,31 @@ function getLocation() {
             reject("Geolocation is not supported by this browser.");
         }
     })
-}
 
-getLocation().then((coords) => {
-    latt = coords.coords.latitude;
-    long = coords.coords.longitude;
-    console.log(latt, long);
+    // get coords of my locstion used Google Geocoder API
+        .then((coords) => {
+            latt = coords.coords.latitude;
+            long = coords.coords.longitude;
+            console.log(latt, long);
 
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latt},${long}&key=AIzaSyAWPYajaSklomoZAVmOuozzvnfK2srbVoM`)
-        .then((response) => {
-
-            locationText.textContent = response.data.results[2].formatted_address;
-
-            axios(`https://api.weatherstack.com/current?access_key=c4db8878e27270c7e233fe2980948cff&query=${latt},${long}`)
+            axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latt},${long}&key=AIzaSyAWPYajaSklomoZAVmOuozzvnfK2srbVoM`)
                 .then((response) => {
-                    console.log(response);
 
-                    temperature.textContent = response.data.current.temperature;
-                    temperature.appendChild(degree);
+                    // show weather of my location
+                    dispayWeather(response);
                 })
+
                 .catch((error) => {
                     console.log(error);
                 });
         })
-        .catch((error) => {
-            console.log(error);
-        });
-})
+}
+// Listen for submit
+locationForm.addEventListener('submit', geocode);
 
+// search for another city
 function geocode(event) {
+
     // Prevent actual submit
     event.preventDefault();
 
@@ -60,27 +62,51 @@ function geocode(event) {
 
             console.log(response);
 
-            // coords
+            // coords for another city
             latt = response.data.results[0].geometry.location.lat;
             long = response.data.results[0].geometry.location.lng;
 
             console.log(latt, long);
 
-            locationText.textContent = response.data.results[0].formatted_address;
+            dispayWeather(response);
+        })
 
-            axios(`https://api.weatherstack.com/current?access_key=c4db8878e27270c7e233fe2980948cff&query=${latt},${long}`)
-                .then((response) => {
-                    console.log(response);
+        .catch((error) => {
+            console.log(error);
+        });
+}
 
-                    temperature.textContent = response.data.current.temperature;
-                    temperature.appendChild(degree);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+function dispayWeather(response) {
+    locationText.textContent = response.data.results[0].formatted_address;
+    axios(`${proxy}http://api.weatherstack.com/current?access_key=c4db8878e27270c7e233fe2980948cff&query=${latt},${long}`)
+        .then((response) => {
+            console.log(response);
+
+            temperature.textContent = response.data.current.temperature;
+            temperatureText.textContent = "Temperature";
+
+            let degree = document.createElement("span");
+            degree.innerHTML = " &deg;C";
+            degree.className = "degree"
+            temperature.appendChild(degree);
+
+            let icon = response.data.current.weather_icons[0];
+            let iconDescription = response.data.current.weather_descriptions[0];
+            weatherIcon.innerHTML = `<img src="${icon}"alt="${iconDescription}">`;
+            weatherDescription.textContent = iconDescription;
+            windSpeed.textContent = response.data.current.wind_speed;
+            windText.textContent = "Wind speed";
+
+            let speed = document.createElement("span");
+            speed.textContent = " km/h";
+            speed.className = "speed";
+            windSpeed.appendChild(speed);
+            
+
 
         })
         .catch((error) => {
             console.log(error);
         });
+
 }
